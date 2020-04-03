7407c473ac842cce1668c6601dd540a76ee85b02
Crow <devcrowmaster@gmail.com>
2020-04-03 00:16:19 -0500

    Initial

---
+++ js/index.js
@@ -0,0 +1,8 @@
+let icon = document.querySelector('.icon');
+let navbar = document.getElementById('menu');
+
+icon.addEventListener('click',()=>{
+  navbar.classList.toggle('open');
+  icon.classList.toggle('open');
+})
+
